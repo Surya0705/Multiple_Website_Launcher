571486c6008e38909faf7e9ea5002520cbe7199d
Surya0705 <77577564+Surya0705@users.noreply.github.com>
2021-01-18 14:06:40 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,6 +5,5 @@ webbrowser.get().open('Website 2')
 webbrowser.get().open('Website 3')
 webbrowser.get().open('Website 4')
 webbrowser.get().open('Website 5')
-webbrowser.get().open('Website 6')
 
 # You can add as many as these lines you want and can open any numbers of tabs at once.

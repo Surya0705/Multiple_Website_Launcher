96a5c66b3504dceebce9bb780b18dd9344fb61c5
Surya0705 <77577564+Surya0705@users.noreply.github.com>
2021-01-18 13:28:49 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+webbrowser.get().open('https://teams.microsoft.com/_?culture=en-in&country=IN&lm=deeplink&lmsrc=homePageWeb&cmpid=WebSignIn#/calendarv2')
+webbrowser.get().open('https://web.whatsapp.com/')
+webbrowser.get().open('https://www.youtube.com/')
+webbrowser.get().open('https://drive.google.com/drive/my-drive')
+webbrowser.get().open('https://colab.research.google.com/drive/1s2d1lwMaKGnM6KTEb2LU3NhxwDnmdqch')
+webbrowser.get().open('https://photos.google.com/')
